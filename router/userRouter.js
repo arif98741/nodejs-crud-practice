5a606b39d6aa927f48bb2733fb51a14378a79e75
Ariful <arif98741@gmail.com>
2025-01-23 11:53:39 +0600

    add necessary routes and test

--- router/userRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { patientRegister } from "../controllers/userController.js";
-
-const router = express.Router();
-router.post("/patient/register", patientRegister);
-
-export default router;
